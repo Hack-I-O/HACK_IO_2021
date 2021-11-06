ded9a9fdc4b56ceaebce3075f756ca7795842f93
Jeremy <Jeremy.yao.info@gmail.com>
2021-11-06 14:34:42 -0400

    read Json from file

--- javascript/DataHelper.js
+++ javascript/DataHelper.js
@@ -1,14 +1,9 @@
 function loadJSONFromFile(pathStr)
 {
-    let returnval;
-    let requestURL = 'data/data.json';
-    let request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        returnval = request.response;
-    }
+    let x;
+    fetch(pathStr)
+        .then(response => response.json())
+        .then(x = json => console.log(json));
 
-    return returnval;
+    return x;
 }
